3e0a8d6f55a7a5e809ecd1e31f5792b1b0597784
AlexanderJaros <59668912+AlexanderJaros@users.noreply.github.com>
2020-02-18 21:47:26 -0500

    complete csv data transfer

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -61,9 +61,20 @@ recent_low = min(low_prices)
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
 with open(csv_file_path, "w") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=["1", "2"])
+        writer = csv.DictWriter(csv_file, fieldnames=["timestamp", "open", "high", "low", "close", "volume"])
         writer.writeheader() #uses fieldnames set above
-        writer.writerow({"1": "New York", "2": "DC"})
+        for date in dates:
+            daily_prices= tsd[date]
+            writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
+            })
+
+
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
